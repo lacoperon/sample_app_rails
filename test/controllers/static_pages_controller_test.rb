e85c259383e0363feac4ad333a6708b03447b1ce
lacoperon <elliot.james.williams@gmail.com>
2017-06-21 19:00:26 -0500

    Added Application Helper function 'full_title', allows us to dynamically produce aesthetically pleasing page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,11 +5,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
-  
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
+
   end
 
   test "should get help" do
@@ -17,7 +18,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
-  
+
   test "should get about" do
     get static_pages_about_url
     assert_response :success
